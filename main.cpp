014d8d89a219b02b779928ad6a7a18db722a6a2c
disha-codes-cpp <dishapatani614@gmail.com>
2026-06-25 19:48:34 +0530

    Added menu function

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void showMenu();
+
+int main()
+{
+    showMenu();
+    return 0;
+}
+void showMenu()
+{
+    cout << "====QUIZ GAME ====" << endl;
+    cout << "1. Start  QUIZ" << endl;
+    cout << "2. Exit" << endl;
+}
+
+
+
